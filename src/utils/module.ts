3cbaa1c4dacb7c9ef66b86de149ae062fa686be3
Byeong <byeong.kim0430@gmail.com>
2024-11-01 13:11:26 -0700

    clean up module-related functions

--- src/utils/module.ts
+++ src/utils/module.ts
@@ -1,67 +1,32 @@
-import { IModuleMountOptions, IModuleMountProps, IModuleRequireProps } from '../typings/types'
-import { REPLServer } from 'repl'
-import { say } from '../modules/cli'
+import { IModuleMountOptions, IModuleRequireProps } from '../typings/types'
+import { buildGlobDirs } from './glob'
 
-export const invalidateCache = (path: string) => {
-  delete require.cache[require.resolve(path)]
-}
+const requireModule = (path: string): NodeRequire => require(path) || {}
 
-export const requireModules = ({ 
-  path, 
-  removeCache = false, 
-  onSuccess,
-  onError
-}: IModuleRequireProps) => {
-  try {
-    if (removeCache) {
-      invalidateCache(path)
+export const modules = {
+  load: function (globPattern: string, onError?: IModuleMountOptions['onError']) {
+    const dirs = buildGlobDirs(globPattern)
+
+    dirs.forEach((dir) => this.require({ path: dir, onError }))
+  },
+  require: function (params: IModuleRequireProps) {
+    try {
+      if (params.removeCache) this.invalidateCache(params.path)
+      const modules = requireModule(params.path)
+      this.mount(modules)
+    } catch (error) {
+      params.onError?.(error as Error)
     }
+  },
+  mount: function (modules: NodeRequire) {
+    const repl = global['_repl']
 
-    const requireModules: NodeRequire = require(path) || {}
-
-    onSuccess(requireModules)
-  } catch (error) {
-    onError?.(error as Error)
-  }
-}
-
-export const mountModulesToServer = ({ 
-  replServer,
-  requiredModules,
-  useGlobal 
-}: IModuleMountProps) => {
-  Object.entries(requiredModules).forEach(([name, module]) => {
-    if (useGlobal) {
-      global[name] = module
-    } else {
-      replServer.context[name] = module
-    }
-  })
-}
-
-export const setModules = ({
-  directories,
-  useGlobal,
-  onError
-}: {
-  directories: string[]
-  useGlobal?: boolean,
-  onError?: IModuleMountOptions['onError']
-}) => (replServer: REPLServer) => {
-  say('mounting export files. please wait...')
-
-  directories.forEach((directory) => {
-    requireModules({
-      path: directory,
-      onSuccess: (requiredModules) => {
-        mountModulesToServer({ replServer, requiredModules, useGlobal })
-      },
-      onError
+    Object.entries(modules || {}).forEach(([name, module]) => {
+      if (repl.useGlobal) global[name] = module
+      else repl.context[name] = module
     })
-  })
-
-  say('done! repl server ready.')
-  say('type .help for more information')
-
-  replServer.displayPrompt()
+  },
+  invalidateCache: function (path: string) {
+    delete require.cache[require.resolve(path)]
+  }
 }
